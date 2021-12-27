2d380434af4ced391a62dd7837e77ccef88ef3e9
Adam Zahradník <adam@zahradnik.xyz>
2021-12-28 00:12:01 +0100

    wait for db before starting

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+echo ">>> Waiting for database"
+WAIT_start=$(date +%s)
+while :
+do
+    (echo -n > /dev/tcp/db/5432) >/dev/null 2>&1
+    if [[ $? -eq 0 ]]; then
+        WAIT_end=$(date +%s)
+        echo "    Database UP after $((WAIT_end - WAIT_start)) seconds"
+        break
+    fi
+    sleep 1
+done
+
 echo ">>> Migrating database"
-python manage.py migrate
+python manage.py migrate --force-color
 
 echo ">>> Starting local server"
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000 --force-color

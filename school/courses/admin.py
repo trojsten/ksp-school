ec9902bc22d687f677ca4997d93535a7c971d023
Adam Zahradník <adam@zahradnik.xyz>
2021-12-28 22:47:39 +0100

    add lesson material models

--- school/courses/admin.py
+++ school/courses/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from school.courses.models import Course, CourseGroup, Lesson
+from school.courses.models import (
+    Course,
+    CourseGroup,
+    Lesson,
+    LessonItem,
+    LessonMaterial,
+)
 
 
 @admin.register(Course)
@@ -13,6 +19,16 @@ class CourseGroupAdmin(admin.ModelAdmin):
     pass
 
 
+class LessonItemInline(admin.TabularInline):
+    model = LessonItem
+    ordering = ["order"]
+
+
 @admin.register(Lesson)
 class LessonAdmin(admin.ModelAdmin):
+    inlines = [LessonItemInline]
+
+
+@admin.register(LessonMaterial)
+class LessonMaterialAdmin(admin.ModelAdmin):
     pass
--- school/courses/models.py
+++ school/courses/models.py
@@ -1,3 +1,5 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.db.models import UniqueConstraint
 
@@ -8,7 +10,7 @@ class Course(models.Model):
         verbose_name_plural = "kurzy"
 
     name = models.CharField(max_length=64)
-    slug = models.CharField(max_length=64, unique=True)
+    slug = models.SlugField(unique=True)
     image = models.ImageField(blank=True, null=True)
     short_description = models.CharField(max_length=200, blank=True)
     description = models.TextField(blank=True)
@@ -40,9 +42,47 @@ class Lesson(models.Model):
 
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     name = models.CharField(max_length=64)
-    slug = models.CharField(max_length=64)
+    slug = models.SlugField()
     layer = models.IntegerField()
     order = models.IntegerField()
 
     def __str__(self):
         return self.name
+
+    def items(self):
+        """
+        Builds QuerySet for retrieving all lesson items with related data
+        :return: QuerySet
+        """
+        return self.lessonitem_set.order_by("order").select_related("lesson_material")
+
+
+class LessonMaterial(models.Model):
+    class Meta:
+        verbose_name = "učebný text"
+        verbose_name_plural = "učebné texty"
+
+    name = models.CharField(max_length=64)
+    content = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class LessonItem(models.Model):
+    class Meta:
+        verbose_name = "časť lekcie"
+        verbose_name_plural = "časti lekcie"
+        constraints = [
+            UniqueConstraint(fields=["lesson", "order"], name="unique_lesson_order"),
+            UniqueConstraint(fields=["lesson", "slug"], name="unique_lesson_slug"),
+        ]
+
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    order = models.IntegerField()
+    slug = models.SlugField(max_length=64)
+
+    lesson_material = models.ForeignKey(
+        LessonMaterial, on_delete=models.CASCADE, blank=True, null=True
+    )
+    # problem = models.ForeignKey("...", on_delete=models.CASCADE, blank=True, null=True)

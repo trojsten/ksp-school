bc42bb2e16dca9975adc4af94c2f80c9987b1bde
Adam Zahradník <adam@zahradnik.xyz>
2022-01-16 01:29:34 +0100

    add fira mono font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        mono: ['"Fira Mono"', ...defaultTheme.fontFamily.mono],
       },
     },
   },

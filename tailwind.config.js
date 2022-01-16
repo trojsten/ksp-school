de1e649d6c1c47a8fd35b15499235f49b31e9b20
Adam Zahradník <adam@zahradnik.xyz>
2022-01-16 01:04:13 +0100

    use Inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./school/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
       require('@tailwindcss/typography'),

b83f7fcaa193ef9f1609da6efe6d1d7e973f6c1b
Adam Zahradník <adam@zahradnik.xyz>
2022-02-01 11:37:39 +0100

    add problem import

--- school/imports/urls.py
+++ school/imports/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from school.imports.views import ImportMaterialsView
+from school.imports.views import ImportMaterialsView, ImportProblemsView
 
 urlpatterns = [
     path("materials/", ImportMaterialsView.as_view()),
+    path("problems/", ImportProblemsView.as_view()),
 ]
--- school/imports/views.py
+++ school/imports/views.py
@@ -11,6 +11,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from school.courses.models import LessonMaterial
 from school.imports.forms import ZipImportForm
+from school.problems.models import Problem
 
 
 @method_decorator(csrf_exempt, name="dispatch")
@@ -59,6 +60,34 @@ class ImportMaterialsView(ImportView):
         orphans = list(
             LessonMaterial.objects.exclude(material_id__in=ids).values_list(
                 "material_id", flat=True
+            )
+        )
+
+        return JsonResponse({"ok": True, "imported": len(ids), "orphans": orphans})
+
+
+class ImportProblemsView(ImportView):
+    def post(self, request, *args, **kwargs):
+        form = ZipImportForm(request.POST, request.FILES)
+
+        if not form.is_valid():
+            data = form.errors.as_json()
+            return JsonResponse({"errors": data, "ok": False}, status=400)
+
+        ids = _import_markdowns(
+            form.cleaned_data["file"],
+            lambda name, meta, body: Problem.objects.update_or_create(
+                testovac_id=name,
+                defaults={
+                    "name": meta.get("name", "???"),
+                    "content": body,
+                },
+            ),
+        )
+
+        orphans = list(
+            Problem.objects.exclude(testovac_id__in=ids).values_list(
+                "testovac_id", flat=True
             )
         )
 

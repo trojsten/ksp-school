1aafd3f588767df0ab52eafa8a01c847dcda3ce8
Adam Zahradník <adam@zahradnik.xyz>
2022-02-01 12:36:34 +0100

    add course import

--- school/imports/urls.py
+++ school/imports/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from school.imports.views import ImportMaterialsView, ImportProblemsView
+from school.imports.views import (
+    ImportCoursesView,
+    ImportMaterialsView,
+    ImportProblemsView,
+)
 
 urlpatterns = [
     path("materials/", ImportMaterialsView.as_view()),
     path("problems/", ImportProblemsView.as_view()),
+    path("courses/", ImportCoursesView.as_view()),
 ]

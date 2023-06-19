baa118e4559c77d7085c146f00fddbfe90f933e8
Adam Zahradník <adam@zahradnik.xyz>
2023-06-19 22:46:57 +0200

    improve homepage

--- school/pages/views.py
+++ school/pages/views.py
@@ -7,6 +7,25 @@ from school.pages.models import Page
 class HomepageView(TemplateView):
     template_name = "pages/home.html"
 
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx[
+            "home_code"
+        ] = """def get_root(node):
+    parent = parents[node]
+    weight = weights[node]
+    if parent == node:
+        return node, 0
+    else:
+        root, parent_weight_to_root = getRoot(parent)
+        weight_to_root = weight + parent_weight_to_root
+        parents[node] = root
+        weights[node] = weight_to_root
+
+        return root, weight_to_root"""
+
+        return ctx
+
 
 class PageView(TemplateView):
     template_name = "pages/page.html"

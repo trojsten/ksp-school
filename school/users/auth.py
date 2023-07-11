4769929850b75b0d1bccb0a0f05c0dca864d009f
Adam Zahradník <adam@zahradnik.xyz>
2023-07-11 21:35:41 +0200

    pair users by trojsten_id, not just emails

--- school/users/auth.py
+++ school/users/auth.py
@@ -1,22 +1,33 @@
+from django.db.models import Q
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
+
+from school.users.models import User
 
 
 class TrojstenOIDCAB(OIDCAuthenticationBackend):
-    def get_username(self, claims):
-        return claims.get("preferred_username")
+    def filter_users_by_claims(self, claims):
+        email = claims.get("email")
+        id = claims.get("sub")
+        if not email and not id:
+            return User.objects.none()
+        return User.objects.filter(Q(email__iexact=email) | Q(trojsten_id=id))
 
     def create_user(self, claims):
-        user = super().create_user(claims)
-
-        user.first_name = claims.get("given_name", "")
-        user.last_name = claims.get("family_name", "")
+        user = User()
+        self._set_user(user, claims)
         user.save()
 
         return user
 
     def update_user(self, user, claims):
-        user.first_name = claims.get("given_name", "")
-        user.last_name = claims.get("family_name", "")
+        self._set_user(user, claims)
         user.save()
 
         return user
+
+    def _set_user(self, user, claims):
+        user.trojsten_id = claims.get("sub")
+        user.email = claims.get("email")
+        user.username = claims.get("preferred_username")
+        user.first_name = claims.get("given_name", "")
+        user.last_name = claims.get("family_name", "")

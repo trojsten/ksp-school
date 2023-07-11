037b1dc829ae16c6052360a411c10144256e418d
Adam Zahradník <adam@zahradnik.xyz>
2023-07-11 22:09:36 +0200

    add SessionRefresh middleware

--- school/settings.py
+++ school/settings.py
@@ -42,6 +42,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "mozilla_django_oidc.middleware.SessionRefresh",
     "django_htmx.middleware.HtmxMiddleware",
 ]
 

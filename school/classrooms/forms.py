453a9b3c021b61be7ec606d5434808e5c60f40ac
Marcel Palaj <palaj.marcel@gmail.com>
2023-07-13 09:48:25 +0200

    add check about multiple ClassroomUser instances

--- school/classrooms/forms.py
+++ school/classrooms/forms.py
@@ -1,11 +1,15 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-from school.classrooms.models import Classroom
+from school.classrooms.models import Classroom, ClassroomUser
 
 
 class JoinForm(forms.Form):
     code = forms.CharField(max_length=8)
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop("request")
+        super(JoinForm, self).__init__(*args, **kwargs)
 
     def clean(self):
         data = super().clean()
@@ -13,6 +17,9 @@ class JoinForm(forms.Form):
         classroom = Classroom.objects.filter(join_code=data["code"]).first()
         if not classroom:
             raise ValidationError("Trieda s daným kódom neexistuje.")
+
+        if ClassroomUser.objects.filter(user=self.request.user, classroom=classroom):
+            raise ValidationError("V tejto triede už si zapísaný/zapísaná.")
 
         data["classroom"] = classroom
 

5588570e481c9f7f9678aa30c8a6ab5dbba9c8fe
Ján Plachý <jan.plachy@trojsten.sk>
2026-07-02 18:25:56 +0200

    Improve texts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,19 @@ module.exports = {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
         mono: ['"Fira Mono"', ...defaultTheme.fontFamily.mono],
       },
+      colors: {
+        'ksp': '#818f3d',
+        'ksp-dark': '#41481f',
+        'ksp-semi-dark': '#565f29',
+        'ksp-light': '#abb47e',
+        'ksp-semi-light': '#9aa564',
+      },
       typography: {
         DEFAULT: {
           css: {
+            'a': {
+              color: '#818f3d',
+            },
             'code::before': {
               content: '',
             },
